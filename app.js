ff935e4142a293d06369e885773110913242e89a
PhongSn <PhongSn@DESKTOP-B3I1M71>
2020-01-14 18:41:27 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
+
+const isAuth = require('./middleware/is-auth');
+
+// const events = [];
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(isAuth);
+
+app.use('/graphql', graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+}));
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-jdqt3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+    app.listen(9000);
+}).catch(err => {
+    console.log(err);
+});
---
+++ graphql/resolvers/events.js
@@ -0,0 +1,51 @@
+const Event = require('../../models/event');
+const User = require('../../models/user');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map((event) => {
+                return transformEvent(event);
+            });
+        } catch (err) {
+            throw err;
+        }
+        // return events;
+    },
+    createEvent: async (args, req) => {
+        // const event = {
+        //     _id: Math.random().toString(),
+        //     title: args.eventInput.title,
+        //     description: args.eventInput.description,
+        //     price: +args.eventInput.price,
+        //     date: args.eventInput.date
+        // };
+        // events.push(event);
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated !');
+        }
+        try {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+                creator: req.userId
+            });
+            let createdEvent;
+            const result = await event.save();
+            createdEvent = transformEvent(result);
+            const creator = await User.findById(req.userId);
+            if (!creator) {
+                throw new Error('User not found.');
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+            return createdEvent;
+        } catch (err) {
+            throw err;
+        }
+    }
+};
